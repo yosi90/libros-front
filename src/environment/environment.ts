fed9d2dc9ccc3fddc36f9732eafb25296f82bc8e
yosi90 <yosi121990@hotmail.es>
2024-05-12 22:16:21 +0100

    ahora usando ip pública

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1/',
+    // apiUrl: 'http://localhost:8080/api/v1/',
+    apiUrl: 'http://85.155.184.126:8081/api/v1/',
 };
