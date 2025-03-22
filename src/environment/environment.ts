b67d0e51f5313b6d23ea59e05af11d78c82fd1e9
yosi90 <yosi121990@hotmail.es>
2025-03-22 21:09:42 +0000

    pues eso..

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5001/',
+    apiUrl: 'http://62.43.222.28:5001/',
     // apiUrl: 'http://85.155.184.126:8081/api/v1/', puerto local a través de ip pública
     // apiUrl: 'https://yosiftware-libros-api.azurewebsites.net/api/v1/',
 };
