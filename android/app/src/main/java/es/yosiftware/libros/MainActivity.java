dd77e1ba66f78c5786110c4b76eb5bf1d4c0c69d
yosi90 <yosi121990@hotmail.es>
2026-08-30 15:14:36 +0100

    chore: retirar diagnóstico WebView innecesario

--- android/app/src/main/java/es/yosiftware/libros/MainActivity.java
+++ android/app/src/main/java/es/yosiftware/libros/MainActivity.java
@@ -1,16 +1,5 @@
 package es.yosiftware.libros;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        if (getPackageName().endsWith(".qa")) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
