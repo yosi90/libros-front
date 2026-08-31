3e3131e988e979f11ce70600c777457d807d79da
yosi90 <yosi121990@hotmail.es>
2026-08-31 10:10:06 +0100

    test(android): enable QA WebView diagnostics

--- android/app/src/main/java/es/yosiftware/libros/MainActivity.java
+++ android/app/src/main/java/es/yosiftware/libros/MainActivity.java
@@ -1,5 +1,16 @@
 package es.yosiftware.libros;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (getPackageName().endsWith(".qa")) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+        super.onCreate(savedInstanceState);
+    }
+}
